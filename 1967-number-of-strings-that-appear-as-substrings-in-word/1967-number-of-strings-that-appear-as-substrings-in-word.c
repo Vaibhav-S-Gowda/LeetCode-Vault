1b2243ae34090d612dc5f8546c472c412794c869
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-06-29 10:48:30 +0530

    Time: 2 ms (8.82%), Space: 9.3 MB (32.35%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+int numOfStrings(char** patterns, int patternsSize, char* word) {
+    int count = 0;
+
+    for (int i = 0; i < patternsSize; i++) {
+        // strstr(main_string, substring)
+        if (strstr(word, patterns[i]) != NULL) {
+            count++;
+        }
+    }
+
+    return count;
+}
