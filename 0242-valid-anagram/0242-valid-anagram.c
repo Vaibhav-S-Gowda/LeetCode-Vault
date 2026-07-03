50bfc4abc45aa1fb6828407c8f9b25cdf00d7fe2
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-07-04 00:44:58 +0530

    Time: 0 ms (100%), Space: 9.1 MB (36.06%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -0,0 +1,17 @@
+bool isAnagram(char* s, char* t) {
+    if (strlen(s) != strlen(t)) {
+        return false;
+    }
+    int count[26] = {0};
+    for(int i = 0; s[i] != '\0'; i++) {
+        count[s[i] - 'a']++;
+    }
+
+    for(int i = 0; t[i] != '\0'; i++) {
+        count[t[i] - 'a']--;
+        
+        if(count[t[i] - 'a'] < 0) return false;
+    }
+
+    return true;
+}
