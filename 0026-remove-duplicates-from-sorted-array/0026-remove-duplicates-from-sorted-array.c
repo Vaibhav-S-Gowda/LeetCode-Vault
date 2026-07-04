beee52493427ee42fcc22dc6a37b52a1961b3265
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-07-04 21:55:45 +0530

    Time: 0 ms (100%), Space: 12.8 MB (95.66%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,13 @@
+int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize == 0) return 0;
+    int i = 0, j = 0;
+
+    while (j < numsSize) {
+        if (nums[i] == nums[j]) {
+            j++;
+        } else {
+            nums[++i] = nums[j];
+        }
+    }
+    return i + 1;
+}
