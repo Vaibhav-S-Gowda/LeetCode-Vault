7ac57c5a8be7f2b1a3603a10f05ab7fe028e7c68
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-07-06 10:33:14 +0530

    Time: 23 ms (87.08%), Space: 9.6 MB (47.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.c
+++ 0322-coin-change/0322-coin-change.c
@@ -1,23 +1,23 @@
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#include <limits.h>
 
 int coinChange(int* coins, int coinsSize, int amount) {
-    int* dp = (int*)malloc((amount + 1) * sizeof(int));
+    int dp[10001];
 
     for (int i = 0; i <= amount; i++) {
-        dp[i] = amount + 1;
+        dp[i] = amount + 1;   // Infinity
     }
 
     dp[0] = 0;
 
     for (int i = 1; i <= amount; i++) {
         for (int j = 0; j < coinsSize; j++) {
-            if (i >= coins[j]) {
-                dp[i] = MIN(dp[i], dp[i - coins[j]] + 1);
+            if (coins[j] <= i) {
+                if (dp[i - coins[j]] + 1 < dp[i]) {
+                    dp[i] = dp[i - coins[j]] + 1;
+                }
             }
         }
     }
 
-    int result = dp[amount];
-    free(dp);
-    return result > amount ? -1 : result;
+    return (dp[amount] > amount) ? -1 : dp[amount];
 }
