8740d89789aa699a009a13bd0c8d7fa08b83418f
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-07-07 22:58:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (33.48%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.c
+++ 0319-bulb-switcher/0319-bulb-switcher.c
@@ -1,11 +1,3 @@
 int bulbSwitch(int n) {
-    if (n == 0) return 0;
-    int result = 1;
-
-    while (result <= n) {
-        if (result*result == n) return result;
-        else if (result*result < n) result++;
-        else return result - 1;
-    }
-    return result;
+    return sqrt(n);
 }
