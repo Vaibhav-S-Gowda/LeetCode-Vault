c86e68993205b814ae0419216f28d5d667e8e9c6
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-07-10 20:36:22 +0530

    Time: 0 ms (100%), Space: 12.5 MB (36.94%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isValid(self, s):
+        # An odd-length string can never be valid
+        if len(s) % 2 != 0:
+            return False
+            
+        # Map closing brackets to their corresponding opening brackets
+        bracket_map = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        
+        stack = []
+        
+        for char in s:
+            # If it's a closing bracket
+            if char in bracket_map:
+                # Pop the top element if stack is not empty, else use a dummy value
+                top_element = stack.pop() if stack else '#'
+                
+                # If the mapping doesn't match the popped element, it's invalid
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it onto the stack
+                stack.append(char)
+                
+        # If the stack is empty, all brackets were matched correctly
+        return len(stack) == 0
+        
