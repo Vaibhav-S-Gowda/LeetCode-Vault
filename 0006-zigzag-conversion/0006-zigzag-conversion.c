66d48fbccd17cdcd4fdde8e45ae3d1e20c93a454
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-08-01 22:11:14 +0530

    Time: 0 ms (100%), Space: 9.6 MB (52.17%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.c
+++ 0006-zigzag-conversion/0006-zigzag-conversion.c
@@ -1,42 +1,35 @@
-char* convert(char* s, int numRows) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* convert(char* s, int numRows) 
+{
     int len = strlen(s);
+   
+    if (numRows == 1 || len <= numRows) 
+    {
+        char* result = (char*)malloc((len + 1) * sizeof(char));
+        strcpy(result, s);
+        return result;
+    }
 
-    if (numRows <= 1 || numRows >= len) return s;
-
-    char** rows = malloc(numRows * sizeof(char*));
-    int* rowLens = calloc(numRows, sizeof(int));
+    char* result = (char*)malloc((len + 1) * sizeof(char));
+    int index = 0;
+    int cycleLen = 2 * numRows - 2;
 
     for (int i = 0; i < numRows; i++) {
-        rows[i] = malloc((len + 1) * sizeof(char));
-    }
-
-    int currentRow = 0;
-    int goingDown = 0;
-
-    for (int i = 0; i < len; i++) {
-        int pos = rowLens[currentRow];
-        rows[currentRow][pos] = s[i];
-        rowLens[currentRow]++;
-
-        if (currentRow == 0 || currentRow == numRows - 1) {
-            goingDown = !goingDown;
+        for (int j = 0; j + i < len; j += cycleLen) 
+        {
+            
+            result[index++] = s[j + i];
+           
+            if (i != 0 && i != numRows - 1 && j + cycleLen - i < len) 
+            {
+                result[index++] = s[j + cycleLen - i];
+            }
         }
-        currentRow += goingDown ? 1 : -1;
     }
 
-    char* result = malloc((len + 1) * sizeof(char));
-    int resultIdx = 0;
-
-    for (int i = 0; i< numRows; i++) {
-        rows[i][rowLens[i]] = '\0';
-        strcpy(result + resultIdx, rows[i]);
-        resultIdx += rowLens[i];
-        free(rows[i]);
-    }
-
-    free(rows);
-    free(rowLens);
-    
     result[len] = '\0';
     return result;
 }
