7cb9af5bcafd639122d523fcbdddba3023e76b48
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-08-03 00:01:11 +0530

    Time: 8 ms (6.01%), Space: 13.5 MB (20.56%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -1,30 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode dummy;
-    dummy.val = 0;
-    dummy.next = NULL;
-    struct ListNode* curr = &dummy; // This pointer will move as we add nodes
-    
-    int carry = 0;
-    
-    while (l1 != NULL || l2 != NULL || carry != 0) {
-        int sum = carry;
-        
-        if (l1 != NULL) {
-            sum += l1->val;
-            l1 = l1->next; 
+    struct ListNode *tail=&dummy;
+    dummy.next=NULL;
+    int carry=0;
+    while(l1!=NULL||l2!=NULL||carry)
+    {
+        int sum=carry;
+        if(l1) {
+            sum+=l1->val;
+            l1=l1->next;
         }
-        if (l2 != NULL) {
-            sum += l2->val;
-            l2 = l2->next; 
+        if(l2) {
+            sum+=l2->val;
+            l2=l2->next;
         }
-        
-        carry = sum / 10; 
-
-        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-        newNode->val = sum % 10;
-        newNode->next = NULL;
-        curr->next = newNode;
-        curr = curr->next;
+        carry=sum/10;
+        tail->next=(struct ListNode*)malloc(sizeof(struct ListNode));
+        tail=tail->next;
+        tail->val=sum%10;
+        tail->next=NULL;
     }
-    return dummy.next; 
+    return dummy.next;
 }
